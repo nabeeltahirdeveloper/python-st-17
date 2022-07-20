593560f4fd90deb08aeed2ae53cfffdb97bfd0dd
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-07-20 22:35:31 +0500

    day 2

---
+++ day 2 - integer float handling/main.py
@@ -0,0 +1,26 @@
+# =====================================Integer
+
+firstVar = 55
+second_var = 45
+print(type(firstVar))
+print(firstVar+second_var)
+
+
+# ===============arithmetic operators
+
+sum_var = firstVar+second_var
+minus_var = firstVar-second_var
+multiply_var = firstVar*second_var
+divide_var = firstVar/second_var
+
+final_var = f"{sum_var}{minus_var} {multiply_var} {divide_var}"
+print("final_var:", type(final_var))
+
+
+nominator = 20
+denominator = 4
+print(type(nominator/denominator))
+
+# ========================floating point
+float_var = 45.351
+print("float type:", type(nominator+float_var), nominator+float_var)
