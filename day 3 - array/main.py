5a97edf10e9fa863a369d21769c4357b903bb73b
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-07-21 23:03:04 +0500

    day 2

---
+++ day 3 - array/main.py
@@ -0,0 +1,29 @@
+# =================================Array=================
+first_var = ["bye", "world", 5, 15.5, "new", "new2"]
+
+# print("first_var:", first_var)
+# print("first_var type:", type(first_var))
+
+# print("just hello:", first_var[0])
+# print("index from 0 to 2:", first_var[::-1])
+
+# # print(varialbe_name[starting_index:ending_index:direction])
+
+# print(first_var[:1:-1])
+
+
+# =======================Conversoin
+
+string_var = "5"
+new_integer = int(string_var)
+
+print(type(new_integer))
+
+integer_var = 55
+new_string = str(integer_var)
+print(type(new_string))
+
+
+new_hello = "5akslfja6ywklajfk"
+print(len(new_hello))
+print(type(len(new_hello)))
