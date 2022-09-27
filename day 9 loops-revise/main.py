37b917a5b291ea41530879e00ad6ef1b6e49e82a
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-09-27 23:30:07 +0500

    loop revise

---
+++ day 9 loops-revise/main.py
@@ -0,0 +1,18 @@
+emails=["nabeel@gmail.com", "test@gmail.com", "data@yahoo.com"]
+
+print(emails)
+
+for email in emails:
+    print(email)
+
+
+for i in range(1,100):
+    print(i)
+
+while 5<10:
+    print("hello")
+
+
+# TODO:while always use condition 
+# syntax should be 
+# while condition:
