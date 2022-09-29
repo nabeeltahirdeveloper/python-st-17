59ff0755e24e82687dc55fbf4d3a84516147c462
nabeeltahirdeveloper <nabeeltahirdeveloper@gmail.com>
2022-09-29 23:34:16 +0500

    day 11

---
+++ day 10 - file handling/main.py
@@ -0,0 +1,17 @@
+file=open("newText.txt", "w")
+file.write("hello worldaa4 \n")
+file.write("hello worldaa4 \n")
+file.write("hello worldaa4 \n")
+file.close()
+
+
+
+readFile=open("newText.txt", 'r')
+print(readFile.readline())
+readFile.close()
+
+
+# print("hello \t world \n 3.0")
+
+
+
